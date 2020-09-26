e432450ab9f3b98a1bc59144d704def57e733361
masayado <masayado@uc.cl>
2020-09-26 20:49:52 -0300

    configure proyecto en vue con 1 componente

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<template>
+    <div id="app">
+    <randomgifcat></randomgifcat>
+    </div>
+</template>
+
+<script>
+import RandomGifCat from "./components/RandomGifComponent.vue"
+export default {
+    name: 'component-name',
+    // props: {},
+    data: function(){
+        return {
+            message:'Random Gif Cat',
+        }
+    },
+    // computed: {},
+    methods: {
+        // -- Metodos
+    },
+    components: {
+        'randomgifcat':RandomGifCat,
+    },
+}
+</script>
+
+<style>
+
+body{
+    margin:0;
+    padding:0;
+    box-sizing: border-box;
+     font-family: 'Montserrat', sans-serif;
+}
+
+</style>
---
+++ src/components/RandomGifComponent.vue
@@ -0,0 +1,47 @@
+<template>
+<div>
+    <div class="randomgifcat">
+        <h1> Random Gif Cat</h1>
+    </div>
+
+      <section class="form">
+        <label for="titulo">Titulo: </label><br>
+        <input type="text" id="titulo" name="titilo" placeholder="Titulo Gif"><br>
+        <label for
+    </section>
+
+</div>
+</template>
+
+<script>
+export default {
+    name: 'randomgifcat-component',
+    // props: {},
+    data: function(){
+        return {}
+    },
+    // computed: {},
+    methods: {
+        // -- Metodos
+    },
+    //components: {},
+}
+</script>
+
+<style scoped>
+    .randomgifcat{
+        padding:15px;
+        margin:0 auto;
+        background-color:lightblue;
+        font-family: 'Montserrat', sans-serif;
+        text-align: center;
+    }
+
+    .form{
+        padding:15px;
+        margin:0 auto;
+        background-color:pink;
+        display:flex;
+        justify-content: center;
+    }
+</style>
