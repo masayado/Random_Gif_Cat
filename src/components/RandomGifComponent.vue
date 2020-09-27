0a037f5f822206e6c6d99c46a26a4b0a39903370
masayado <masayado@uc.cl>
2020-09-27 15:39:13 -0300

    termine maqueta sitio

--- src/components/RandomGifComponent.vue
+++ src/components/RandomGifComponent.vue
@@ -1,15 +1,49 @@
 <template>
-<div>
+<div class="main-container">
     <div class="randomgifcat">
         <h1> Random Gif Cat</h1>
     </div>
+    <div class="d-flex justify-content-center align-items-center p-4 " style="background-color:pink;">
+        <form>
+    
+        <div class="form-group row">
+            <label for="Titulo" class="col-sm-2 col-form-label"><small>Titulo</small></label>
+            <div class="col-sm-10">
+            <input type="title" class="form-control" id="" placeholder="Titulo">
+            </div>
+        </div>
 
-      <section class="form">
-        <label for="titulo">Titulo: </label><br>
-        <input type="text" id="titulo" name="titilo" placeholder="Titulo Gif"><br>
-        <label for
-    </section>
+        <div class="form-group row">
+            <label for="Filtro" class="col-sm-2 col-form-label"><small>Filtro</small></label>
+            <div class="col-sm-10">
+            <select class="form-control" id="exampleFormControlSelect1">
+            <option>1</option>
+            </select>
+            </div>
+        </div>
 
+        <div class="form-group row">
+            <label for="Filtro" class="col-sm-2 col-form-label"><small>Color</small></label>
+            <div class="col-sm-10">
+            <select class="form-control" id="exampleFormControlSelect1">
+            <option>1</option>
+            </select>
+            </div>
+        </div>
+
+        <div class="form-group row">
+            <label for="Titulo" class="col-sm-2 col-form-label"><small>Tamaño</small></label>
+            <div class="col-sm-10">
+            <input type="title" class="form-control" id="" placeholder="Tamaño">
+            </div>
+        </div>
+        </form>
+        </div>
+
+    <div class="randomgifcat">
+        <button type="button" class="btn btn-light" @click="ObtenerGato">Obtener Cat Gif</button>
+        <img src="" alt="">
+    </div>
 </div>
 </template>
 
@@ -22,7 +56,9 @@ export default {
     },
     // computed: {},
     methods: {
-        // -- Metodos
+        ObtenerGato:function(){
+            alert("Obteniendo gatito")
+        }
     },
     //components: {},
 }
@@ -33,15 +69,8 @@ export default {
         padding:15px;
         margin:0 auto;
         background-color:lightblue;
-        font-family: 'Montserrat', sans-serif;
         text-align: center;
+        height: 100%;
     }
 
-    .form{
-        padding:15px;
-        margin:0 auto;
-        background-color:pink;
-        display:flex;
-        justify-content: center;
-    }
 </style>
