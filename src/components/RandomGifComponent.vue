76ecb2eef4b1a631b7566a17e4a77e970e3acac6
masayado <masayado@uc.cl>
2020-09-27 22:00:44 -0300

    agregue logica para aplicar filtros al gif

--- src/components/RandomGifComponent.vue
+++ src/components/RandomGifComponent.vue
@@ -7,16 +7,16 @@
         <form>
     
         <div class="form-group row">
-            <label for="title" class="col-sm-2 col-form-label" v-model="titulo"><small>Titulo</small></label>
+            <label for="title" class="col-sm-2 col-form-label"><small>Titulo</small></label>
             <div class="col-sm-10">
-            <input type="text" class="form-control" id="" placeholder="Titulo">
+            <input type="text" class="form-control" id="" placeholder="Titulo" v-model="titulo">
             </div>
         </div>
 
         <div class="form-group row">
             <label for="filter" class="col-sm-2 col-form-label"><small>Filtro</small></label>
             <div class="col-sm-10">
-            <select class="form-control" id="exampleFormControlSelect1">
+            <select class="form-control" id="exampleFormControlSelect1" @change="changeFilter($event)">
             <option 
             v-for="filtro in filtros" 
             :key="filtro" 
@@ -30,7 +30,7 @@
         <div class="form-group row">
             <label for="color" class="col-sm-2 col-form-label"><small>Color</small></label>
             <div class="col-sm-8">
-            <select class="form-control" id="exampleFormControlSelect1" @change="cambioColor($event)">
+            <select class="form-control" id="exampleFormControlSelect1" @change="changeColor($event)">
             <option 
             v-for="color in colores" 
             :key="color.color" 
@@ -53,10 +53,11 @@
         </form>
         </div>
 
-    <div class="randomgifcat">
+    <div class="gifcat">
         <button type="button" class="btn btn-light" @click="GetGifCat">Obtener Gif Cat</button>
         
     <div class="gifimg">
+        <img :src="gifurl">
     </div>
 
     </div>
@@ -82,21 +83,52 @@ export default {
             ],
             dotStyle:{
                 background:"",
-            }
+            },
+            gifurl:"",
+            selected:"",
         }
     },
     // computed: {},
     methods: {
-        GetGifCat:function(){
-        alert("Obteniendo gatito")
-        fetch(`https://cataas.com/cat/gif/says/${this.titulo}?filter=filtro&color=color&size=tamaño`)
-        .then(response => response.json())
-        .then(data => console.log(data));
-        },
-        cambioColor(e){
+        changeColor(e){
             //alert(e.target.value); //estoy capturando el evento
             this.dotStyle.background = e.target.value
+        },
+        changeFilter(e){
+            //alert(e.target.value); //capturando evento
+            var resp = e.target.value;
+            if(resp === "blur"){
+                this.selected = "blur"
+            }
+            else if(resp === "mono"){
+                this.selected = "mono"
+            }
+            else if(resp === "sepia"){
+                this.selected = "sepia"
+            }
+            else if(resp === "negative"){
+                this.selected = "negative"
+            }
+            else if(resp === "paint"){
+                this.selected = "paint"
+            }
+            else if(resp === "pixel"){
+                this.selected = "pixel"
+            }
+            return
+            selected, gifurl
+        },
+        GetGifCat:function(){
+        //alert("Obteniendo gatito")
+        if(this.titulo === ""){
+            alert("Ingresa un título")
         }
+        else if(this.tamano === ""){
+            alert("Ingresa tamaño de fuente")
+        }
+        else
+            this.gifurl=`https://cataas.com/cat/gif/says/${this.titulo}?filter=${this.selected}&color=${this.dotStyle.background}&size=${this.tamano}&type=or`
+        },
     },
     //components: {},
 }
@@ -109,6 +141,15 @@ export default {
         background-color:lightblue;
         text-align: center;
         height: 100%;
+    }
+
+    .gifcat{
+        padding:15px;
+        margin:0 auto;
+        background-color:lightblue;
+        text-align: center;
+        height: auto;
+        min-height: 300px;
     }
 
     .gifimg{
